a02b9ac9976467f44e40d09d3548c2ad2316120e
David <hsienwu830808@gmail.com>
2020-05-15 06:19:37 +0800

    Add drink products in discountStrategy.ts

--- strategyPattern/discountStrategy.ts
+++ strategyPattern/discountStrategy.ts
@@ -42,3 +42,40 @@ class NoneStrategy extends DiscountStrategy {
     }
     
 }
+
+abstract class Drink {
+    discountStrategy: DiscountStrategy = new NoneStrategy(1)
+    value: number
+
+    constructor(value: number, discount: DiscountStrategy) {
+        this.value = value
+        this.discountStrategy = discount
+    }
+
+    getValue(): number {
+        return this.discountStrategy.getValue(this.value)
+    }
+}
+
+class MilkTea extends Drink {
+
+    constructor(value: number, discount: DiscountStrategy) {
+        super(value, discount)
+    }
+
+    getValue(): number {
+        return super.getValue()
+    }
+}
+
+class Coffee extends Drink {
+
+    constructor(value: number, discount: DiscountStrategy) {
+        super(value, discount)
+    }
+
+    getValue(): number {
+        return super.getValue()
+    }
+    
+}
