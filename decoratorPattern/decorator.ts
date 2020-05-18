678a703e06f5fe1e386121c80c673761e73362b7
David <hsienwu830808@gmail.com>
2020-05-18 21:18:55 +0800

    Add combo to decorator.ts

--- decoratorPattern/decorator.ts
+++ decoratorPattern/decorator.ts
@@ -32,3 +32,52 @@ class BusinessLunch extends Restaurant {
     }
 
 }
+
+class SimpleCombo extends BusinessLunch {
+
+    constructor(restaurant: Restaurant) {
+        super(restaurant)
+    }
+
+    soup(): void {
+        console.log('一份湯品');
+    }
+
+    order(): void {
+        super.order()
+        this.soup()
+    }
+}
+
+class FullCombo extends SimpleCombo {
+    constructor(restaurant: Restaurant) {
+        super(restaurant)
+    }
+
+    sweet(): void {
+        console.log('一份甜點')
+    }
+
+    order(): void {
+        super.order()
+        this.sweet()
+    }
+
+}
+
+const restaurant : Restaurant = new Restaurant()
+
+const simpleCombo: SimpleCombo = new SimpleCombo(restaurant)
+
+const businessLaunch: BusinessLunch = new BusinessLunch(restaurant)
+
+const fullCombo: FullCombo = new FullCombo(restaurant)
+
+console.log('簡餐')
+simpleCombo.order()
+
+console.log('商業午餐')
+businessLaunch.order()
+
+console.log('全餐')
+fullCombo.order()
